837fd2ce946abc299cf78b79be6216fd4c6bd92e
Elijah Plushkov <134138197+ElijahPlushkov@users.noreply.github.com>
2025-06-28 10:48:45 +0300

    Delete user-profile-display.php
    moved the function to user-profile.controller.php

--- user-profile-display.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class UserProfileDisplay extends ProfileInfo {
-    public function fetchAbout($id) {
-        $profileInfo = $this->getProfileInfo($id);
-
-        echo $profileInfo["about"];
-    }
-}

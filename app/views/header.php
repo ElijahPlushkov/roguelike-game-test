d55411b92fb4aa0989a1c59191617dddb11de075
Elijah Plushkov <134138197+ElijahPlushkov@users.noreply.github.com>
2025-07-14 19:49:08 +0300

    cleaned up some code

--- app/views/header.php
+++ app/views/header.php
@@ -1,16 +1,3 @@
-<?php
-session_start();
-echo "<pre>";
-print_r($_SESSION);
-echo "</pre>";
-if (isset($_SESSION["id"])) {
-    echo "<p>Welcome, ".htmlspecialchars($_SESSION["username"])."! You are logged in.</p>";
-    unset($_SESSION["error"]);
-} else {
-    echo "<p>You are not logged in</p>";
-}
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
